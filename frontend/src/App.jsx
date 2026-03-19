df9515664b129d0e80feea38fd13ea36858c4a1b
ODD SOUL <vijivenkadesh@gmail.com>
2026-03-19 07:10:51 +0530

    Front end initial setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import ChatButton from './components/ChatButton'
+import '../style/App.css'
+import '../style/ChatButton.css'
+
+function App() {
+
+
+  return (
+    <>
+      <ChatButton></ChatButton>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/ChatButton.jsx
@@ -0,0 +1,20 @@
+function ChatButton() {
+    return <div className="chat-button-container">
+        <input
+        type="text"
+        placeholder="Hello How can I help you today?"
+        className="chat-input"
+        />
+        <button className="chat-button">Chat</button>
+    </div>
+}
+
+
+
+
+
+
+
+
+
+export default ChatButton;
